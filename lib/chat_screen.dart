b495e14a88ad0a94ad8f238a3fd9f3dd8aabc6c4
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-19 12:01:56 -0300

    Gravando a msg no firebase.

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_firebase/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -7,6 +8,12 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _sendMessage(String text) {
+    Firestore.instance.collection("messages").add({
+      'text': text,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +21,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("Olá"),
         elevation: 0,
       ),
-      body: TextComposer((text) {
-        print(text);
-      }),
+      body: TextComposer(_sendMessage),
     );
   }
 }
